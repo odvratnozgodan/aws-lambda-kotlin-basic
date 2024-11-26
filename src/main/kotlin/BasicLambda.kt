2d47b588ec60286de9cca4a18eb4603105fe4415
Bruno Blazinc <odvratno.zgodan@gmail.com>
2024-11-26 10:57:26 +0100

    Fix formating

--- src/main/kotlin/BasicLambda.kt
+++ src/main/kotlin/BasicLambda.kt
@@ -8,13 +8,15 @@ import solutions.deliverit.models.ResponseOutput
 
 
 class BasicLambda : RequestHandler<RequestInput, ResponseOutput> {
-    override fun handleRequest(request: RequestInput?, context: Context): ResponseOutput {
+    override fun handleRequest(
+        request: RequestInput?,
+        context: Context
+    ): ResponseOutput {
         val logger: LambdaLogger = context.logger
         logger.log("ECHO INPUT RECEIVED: " + request?.message)
-        return when(request){
+        return when (request) {
             null -> ResponseOutput("No input provided.")
             else -> ResponseOutput("Received message: ${request.message}")
         }
     }
-
 }
